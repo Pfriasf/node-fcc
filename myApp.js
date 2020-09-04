8a026df1d9c5ad44fce7508c1a9593c79fcdb179
Pedro Frias <plfriasf@gmail.com>
2020-09-04 08:47:35 -0700

    exercise 11

--- myApp.js
+++ myApp.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var app = express();
-
+var bodyParser = require("body-parser");
 // --> 7)  Mount the Logger middleware here
 app.use(function (req, res, next) {
   let string = req.method + " " + req.path + " " + "-" + " " + req.ip;
@@ -9,6 +9,8 @@ app.use(function (req, res, next) {
 });
 
 // --> 11)  Mount the body-parser middleware  here
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 /** 1) Meet the node console. */
 
